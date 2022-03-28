f98149b46447f0fab35bd1cd86db4f89de8d8fb3
hannahmholloway <hholl002@odu.edu>
2022-03-28 13:09:44 -0400

    Added firebase dependencies

--- app/src/main/java/com/example/havis/LoginActivity.java
+++ app/src/main/java/com/example/havis/LoginActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -15,7 +18,142 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Timer;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    private static final int MY_PERMISSIONS_CAMERA = 1;
+    private ImageView logo, ivSignIn, btnTwitter;
+    private AutoCompleteTextView email, password;
+    private TextView forgotPass, signUp;
+    private Button btnSignIn;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initializeGUI();
+
+        //This block of code automatically signs in whoever was using the app previously.
+        //While useful, Im removing it for now so we know exactly who is signing in for testing purposes.
+        /*user = firebaseAuth.getCurrentUser();
+
+        if(user != null) {
+            finish();
+            startActivity(new Intent(LoginActivity.this,Homepage.cla ss));
+        }*/
+
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String inEmail = email.getText().toString();
+                String inPassword = password.getText().toString();
+
+                if(validateInput(inEmail, inPassword))
+                {
+                    signUser(inEmail, inPassword);
+                }
+                else
+                {
+                    email.setError("Incorrect email or password.");
+                }
+
+            }
+        });
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SplashScreen.class));
+            }
+        });
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SplashScreen.class));
+            }
+        });
+
+
+
+    }
+
+
+
+    public void signUser(String email, String password){
+
+        progressDialog.setMessage("Logging In...");
+        progressDialog.show();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(LoginActivity.this,LoginActivity.class));
+                    //startActivity(new Intent(LoginActivity.this,Homepage.class));
+                }
+                else{
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this,"Invalid email or password",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
+    private void initializeGUI(){
+
+        logo = findViewById(R.id.ivLogLogo);
+        email = findViewById(R.id.atvEmailLog);
+        password = findViewById(R.id.atvPasswordLog);
+        forgotPass = findViewById(R.id.tvForgotPass);
+        signUp = findViewById(R.id.tvSignIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+
+    public boolean validateInput(String inemail, String inpassword){
+
+        if(inemail.isEmpty()){
+            email.setError("Email field is empty.");
+            return false;
+        }
+        if(inpassword.isEmpty()){
+            password.setError("Password is empty.");
+            return false;
+        }
+
+        return true;
+    }
 
 }
